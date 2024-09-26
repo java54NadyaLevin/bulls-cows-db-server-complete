a6a256511377cebe8aa96915fa725c2b93038a4f
Yuragranovsky <yuragranovsky@gmail.com>
2024-09-26 19:17:30 +0300

    updates tests

--- src/test/java/telran/net/games/service/BullsCowsServiceTest.java
+++ src/test/java/telran/net/games/service/BullsCowsServiceTest.java
@@ -42,6 +42,8 @@ class BullsCowsServiceTest {
 	void createGameTest() {
 		gameIdNormalFlow = bcService.createGame();
 		Game game = repository.getGame(gameIdNormalFlow);
+		assertNull(game.getDate());
+		assertFalse(game.isfinished());
 		toBeGuessedSequence = ((BullsCowsServiceImpl)bcService).getSequence(gameIdNormalFlow);
 		assertTrue(bcRunner.checkGuess(toBeGuessedSequence));
 		
@@ -106,7 +108,7 @@ class BullsCowsServiceTest {
 				bcService.moveProcessing(toBeGuessedSequence,
 						gameIdNormalFlow, username);
 		MoveData moveDataExpected = new MoveData(toBeGuessedSequence,
-				4, 0);
+				N_DIGITS, 0);
 		assertEquals(2, moves.size());
 		assertEquals(moveDataExpected, moves.get(1));
 		assertTrue(repository.isGameFinished(gameIdNormalFlow));
